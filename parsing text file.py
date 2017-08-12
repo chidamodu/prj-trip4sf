cbb97d657ab1acd496a5fec4a50cf34bae465eae
chidamodu <chidambaram.spv@gmail.com>
2017-08-12 15:31:55 -0700

    Update parsing text file.py

--- parsing text file.py
+++ parsing text file.py
@@ -1,3 +1,3 @@
 import pandas as pd
-g= pd.read_csv('codeforsf-updatedfile.txt', sep='\n', header=None) #converted the PDF file to a text file (codeforsf-updatedfile.txt). separating each line, while reading, at newline character.
+g= pd.read_csv('codeforsf-updatedfile.txt', sep='\n', header=None) #converted the PDF file to a text file (codeforsf-updatedfile.txt). separating each line, while reading as rows, at newline character.
 df=pd.DataFrame(g) #creating a dataframe of the input file.
